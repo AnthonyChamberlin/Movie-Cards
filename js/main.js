da53c7d7ad2fe80dc7d5ed9e1b3cf83276577ccf
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-01-25 13:49:31 +0000

    Add Simple JavaScript if/else for selecting and changing style of stars

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function starClick(a) {
+    var element;
+    if (a == 1) {
+        element = document.getElementById('star_1');
+        element.style.color = "#FF912C";
+    } else if (a == 2) {
+        element = document.getElementById('star_2');
+        element.style.color = "#FF912C";
+    } else if (a == 3) {
+        element = document.getElementById('star_3');
+        element.style.color = "#FF912C";
+    } else if (a == 4) {
+        element = document.getElementById('star_4');
+        element.style.color = "#FF912C";
+    } else if (a == 5) {
+      element = document.getElementById('star_5');
+      element.style.color = "#FF912C";
+    } else return;
+}
