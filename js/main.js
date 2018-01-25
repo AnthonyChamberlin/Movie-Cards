40d0835fd7fc09301cd26dc6e7af3e8bb86e45c1
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-01-25 15:39:52 +0000

    add movie cards

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
-function rateClick1(button1) {
-    var x = button1.value;
-    document.getElementById("result1").innerHTML = "Your Rating: " + x + "/10";
+function rateClick1(button) {
+    var a = button.value;
+    document.getElementById("result1").innerHTML = "Your Rating: " + a + "/10";
 }
 
-function rateClick2(button2) {
-    var y = button2.value;
-    document.getElementById("result2").innerHTML = "Your Rating: " + y + "/10";
+function rateClick2(button) {
+    var b = button.value;
+    document.getElementById("result2").innerHTML = "Your Rating: " + b + "/10";
 }
 
-function rateClick3(button3) {
-    var z = button3.value;
+function rateClick3(button) {
+    var c = button.value;
     var result = document.getElementById("result3");
+    result.innerHTML = "Your Rating: " + c + "/10";
+
+}
+
+function rateClick4(button) {
+    var x = button.value;
+    document.getElementById("result4").innerHTML = "Your Rating: " + x + "/10";
+}
+
+function rateClick5(button) {
+    var y = button.value;
+    document.getElementById("result5").innerHTML = "Your Rating: " + y + "/10";
+}
+
+function rateClick6(button) {
+    var z = button.value;
+    var result = document.getElementById("result6");
     result.innerHTML = "Your Rating: " + z + "/10";
 
 }
